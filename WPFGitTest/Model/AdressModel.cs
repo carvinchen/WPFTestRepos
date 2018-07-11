15caee8b1164ec9d9b93fac07dec90383f945b90
WEI_PRIVATE\knigh_001 <huboode@googlemail.com>
2018-07-11 20:03:48 +0200

    Complete model and viewmodel files for this test project.

---
+++ WPFGitTest/Model/AdressModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFGitTest.Model
+{
+    public class AdressModel
+    {
+
+        private string _id;
+        private string _street;
+        private string _houseNr;
+        private string _zipCode;
+        private string _city;
+    }
+}
---
+++ WPFGitTest/ViewModel/AdressViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using WPFGitTest.Model;
+
+namespace WPFGitTest.ViewModel
+{
+    public class AdressViewModel : INotifyPropertyChanged
+    {
+
+        private AdressModel _adressModel;
+
+        public AdressModel AdressModel {
+
+            get { return _adressModel; }
+            set {
+                if (_adressModel != value)
+                {
+                    _adressModel = value;
+                    OnPropertyChanged("AdressModel");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string info)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
