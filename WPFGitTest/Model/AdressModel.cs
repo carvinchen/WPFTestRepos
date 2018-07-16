c4d433259c6180b102cdcf97e6ed5576a0434591
NatrualSwordGitProject <huboode@googlemail.com>
2018-07-16 11:33:09 +0200

    Update Viewmodel for combobox.

--- WPFGitTest/Model/AdressModel.cs
+++ WPFGitTest/Model/AdressModel.cs
@@ -10,14 +10,15 @@ namespace WPFGitTest.Model
     {
 
         private string _id;
+        private string _firstname;
+        private string _lastname;
         private string _street;
-        private string _houseNr;
+        private int _houseNr;
         private string _zipCode;
         private string _city;
         private string _country;
 
         public string ID {
-
             get { return _id;  }
             set {
                 if (_id != value)
@@ -27,11 +28,96 @@ namespace WPFGitTest.Model
             }
         }
 
+        public string Firstname
+        {
+            get { return _firstname; }
+            set
+            {
+                if (_firstname != value)
+                {
+                    _firstname = value;
+                }
+            }
+        }
+
+        public string Lastname
+        {
+            get { return _lastname; }
+            set
+            {
+                if (_lastname != value)
+                {
+                    _lastname = value;
+                }
+            }
+        }
+
+        public string Street
+        {
+
+            get { return _street; }
+            set
+            {
+                if (_street != value)
+                {
+                    _street = value;
+                }
+            }
+        }
+
+        public string ZipCode
+        {
+
+            get { return _zipCode; }
+            set
+            {
+                if (_zipCode != value)
+                {
+                    _zipCode = value;
+                }
+            }
+        }
+
+        public string City
+        {
+
+            get { return _city; }
+            set
+            {
+                if (_city != value)
+                {
+                    _city = value;
+                }
+            }
+        }
+
+        public string Country
+        {
+
+            get { return _country; }
+            set
+            {
+                if (_country != value)
+                {
+                    _country = value;
+                }
+            }
+        }
+
+        public int HouseNr {
+
+            get { return _houseNr; }
+            set
+            {
+                if (_houseNr != value)
+                {
+                    _houseNr = value;
+                }
+            } 
+        } 
+
         //C# 6.0 new feature: A initializer to an auto-property DOESNT WORK with VS 2013...
         //public string Country { get; set; } = "Deutschland";
-
-
-
 
 
     }
--- WPFGitTest/ViewModel/AdressViewModel.cs
+++ WPFGitTest/ViewModel/AdressViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,15 @@ namespace WPFGitTest.ViewModel
 {
     public class AdressViewModel : INotifyPropertyChanged
     {
+        public static int[] listOfHouseNumber = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+        public static string filePath = Directory.GetCurrentDirectory();
+        public static string defaultXMLFileName = @"..\..\files\SavingGameData_past.xml";
+        public string filePathName = Path.Combine(filePath, defaultXMLFileName);    
 
         private AdressModel _adressModel;
+        //private string[] _istOfHouseNrStr = new string[listOfHouseNumber.Length];
+        private string[] _listOfHouseNrStr = new string[] { };
 
         public AdressModel AdressModel {
 
@@ -24,6 +32,70 @@ namespace WPFGitTest.ViewModel
                 }
             }
         }
+
+        //Using getter methode
+        public string[] ListOfHouseNr {
+            get {
+                //string[] listOfHouseNrStr = new string[] {};
+
+                if (listOfHouseNumber.Length > 0)
+                {
+                    _listOfHouseNrStr = new string[listOfHouseNumber.Length];
+                    for (int i = 0; i < listOfHouseNumber.Length; i++)
+                    {
+                        _listOfHouseNrStr[i] = listOfHouseNumber[i].ToString();
+                    }
+                }
+                return _listOfHouseNrStr;
+            }
+        }
+
+        //Using LING features
+        public string[] ListOfHouseNrLing {
+
+            get {
+
+                if (listOfHouseNumber.Length > 0)
+                {
+                    _listOfHouseNrStr = new string[listOfHouseNumber.Length];
+                    
+                    //Query creation and only output even number
+                    var houseNrQuery =
+                        from hNum in listOfHouseNumber
+                        where (hNum % 2) == 0
+                        select hNum;
+
+                    if (houseNrQuery.Count<int>() > 0)
+                    {
+                        for(int i = 0; i < houseNrQuery.ToArray().Length; i++){
+                            _listOfHouseNrStr[i] = houseNrQuery.ToArray()[i].ToString();
+                        }
+                    }
+                }
+
+                //TODO: Call XML methode
+
+                return _listOfHouseNrStr;
+            }
+        }
+
+
+        //Function that handle the data from xml files
+
+
+        public int SelectedHouseNumber {
+
+            get { return AdressModel.HouseNr; }
+            set {
+                if (AdressModel.HouseNr != value)
+                {
+                    AdressModel.HouseNr = value;
+                    OnPropertyChanged("SelectedHouseNumber");
+                }
+            }
+        }
+
+        
 
         public event PropertyChangedEventHandler PropertyChanged;
 
